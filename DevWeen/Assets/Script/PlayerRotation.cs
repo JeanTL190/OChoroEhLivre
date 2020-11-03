087e67a14d3c887cf7874b95893f59b234b1b722
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-11-02 22:27:16 -0300

    Rotacionar

---
+++ DevWeen/Assets/Script/PlayerRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string inptRotaciona;
+    [SerializeField] private float angularSpeed = 5f;
+    private float inpt;
+    // Update is called once per frame
+    void Update()
+    {
+        inpt = Input.GetAxisRaw(inptRotaciona);
+        Rotate(this.transform, angularSpeed * inpt);
+    }
+    private void Rotate(Transform t, float angulo)
+    {
+        t.Rotate(0, 0, -angulo);
+    }
+}
