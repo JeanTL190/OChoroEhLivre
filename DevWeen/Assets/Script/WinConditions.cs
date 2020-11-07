bba005888c50fdeae27e46d8225d3838a8bf5f9e
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-11-06 21:42:39 -0300

    Fred

--- DevWeen/Assets/Script/WinConditions.cs
+++ DevWeen/Assets/Script/WinConditions.cs
@@ -1,9 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class WinConditions : MonoBehaviour
 {
-    [SerializeField] private PlayerAtributos[] players;
+    [SerializeField] private float timeReset = 10f;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private PlayerAtributos [] player;
+    [SerializeField] private UI_Manager um;
+    private bool lost = false;
+    private float timeAtual;
+    void Start()
+    {
+        timeAtual = timeReset;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (!lost)
+        {
+            if (timeAtual > 0f)
+            {
+                timeAtual -= Time.deltaTime;
+                timeText.text = timeAtual.ToString("F");
+            }
+            else
+            {
+                if (player[1].GetPoints() > player[2].GetPoints())
+                {
+                    um.W1Button();
+                }
+                else
+                {
+                    um.W2Button();
+                }
+            }
+        }
+    }
 }
